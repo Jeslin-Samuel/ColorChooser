49e8de668f4711ae873c506829ccd40924831f09
Jeslin Samuel <jeslin.samuel@temple.edu>
2019-09-27 20:20:39 -0400

    Added list of colors (all 10 from Color class constants), created objects Spinner and ColorAdapter, and set the spinner to use the ColorAdapter as the adapter.

--- app/src/main/java/edu/temple/colorlab/ColorActivity.java
+++ app/src/main/java/edu/temple/colorlab/ColorActivity.java
@@ -3,12 +3,23 @@ package edu.temple.colorlab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class ColorActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.ListofColors);
+
+        String colors[] = {"Green", "Red", "Blue", "Yellow", "Cyan", "Magenta", "Black", "Gray",
+                "Light Gray", "Dark Gray"};
+
+        ColorAdapter adapter = new ColorAdapter(ColorActivity.this, colors);
+
+        spinner.setAdapter(adapter);
     }
 }
--- app/src/main/java/edu/temple/colorlab/ColorAdapter.java
+++ app/src/main/java/edu/temple/colorlab/ColorAdapter.java
@@ -1,10 +1,21 @@
 package edu.temple.colorlab;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 public class ColorAdapter extends BaseAdapter {
+    String colors[];
+    Context context;
+
+    public ColorAdapter(Context context, String[] colors)
+    {
+        this.context = context;
+        this.colors = colors;
+    }
+
     @Override
     public int getCount() {
         return 0;
