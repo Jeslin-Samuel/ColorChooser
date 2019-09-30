49df770bc9fc918b24502f884d0ffb9d30dcb1fc
Jeslin Samuel <jeslin.samuel@temple.edu>
2019-09-30 18:27:32 -0400

    Added functionality that changes the background of the constraint layout according to the color selected in the spinner dropdown while the chosen color view itself has its background set to white.

--- app/src/main/java/edu/temple/colorlab/ColorActivity.java
+++ app/src/main/java/edu/temple/colorlab/ColorActivity.java
@@ -1,8 +1,11 @@
 package edu.temple.colorlab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
@@ -16,12 +19,27 @@ public class ColorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner spinner = findViewById(R.id.ListofColors);
+        final ConstraintLayout layout = findViewById(R.id.myLayout);
 
-        String colors[] = {"Green", "Red", "Blue", "Yellow", "Cyan", "Magenta", "Black", "Gray",
+        final String colors[] = {"Green", "Red", "Blue", "Yellow", "Cyan", "Magenta", "Black", "Gray",
                 "LightGray", "DarkGray"};
 
         ColorAdapter adapter = new ColorAdapter(ColorActivity.this, colors);
 
         spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
+            {
+                view.setBackgroundColor(Color.WHITE);
+                layout.setBackgroundColor(Color.parseColor(colors[i]));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
